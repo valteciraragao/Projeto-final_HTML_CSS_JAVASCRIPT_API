100a8e015034f617de334e0cddd426338ec03fec
valteciraragao <valteciraragao@poli.ufrj.br>
2023-07-12 17:53:33 -0300

    integração da api com a pagina de mentores

---
+++ javascript/mentores.js
@@ -0,0 +1,18 @@
+const renderMentores = (mentores) => {
+    const table = document.querySelector("tbody")
+    mentores.forEach(mentor => {
+        table.innerHTML = table.innerHTML + 
+        `<tr>
+            <td>${mentor.nome}</td>
+            <td>${mentor.email}</td>
+            <td><button>Alterar</button><button>Excluir</button></td>
+        </tr>`})
+}
+
+const getMentores = async() => {
+    const response = await fetch("http://localhost:3000/mentores")
+    const mentores = await response.json()
+    renderMentores(mentores)
+}
+
+getMentores()
